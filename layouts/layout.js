50ba7e33fb71e7fd77ac4b88470f66db175c1967
Clark Weckmann <15677451+clarkhacks@users.noreply.github.com>
2022-09-04 13:30:12 -0500

    Meta Tags

--- layouts/layout.js
+++ layouts/layout.js
@@ -29,7 +29,7 @@ const Layout = ({
       // date={new Date(frontMatter.publishedAt).toISOString()}
       type="article"
       slug={router.asPath}
-      keywords={frontMatter.tags.map(tag => tag.name).join(', ')}
+      keywords={frontMatter.tags}
       fullWidth={fullWidth}
     >
       <article>
