2553dcec1d9876b3bf15e27c2460d1edd3fbd259
Clark Weckmann <15677451+clarkhacks@users.noreply.github.com>
2022-09-04 13:27:01 -0500

    SPelling

--- layouts/layout.js
+++ layouts/layout.js
@@ -29,7 +29,7 @@ const Layout = ({
       // date={new Date(frontMatter.publishedAt).toISOString()}
       type="article"
       slug={router.asPath}
-      keywords={frontmatter.tags.map(tag => tag.name).join(', ')}
+      keywords={frontMatter.tags.map(tag => tag.name).join(', ')}
       fullWidth={fullWidth}
     >
       <article>
