d2a364e2c446b8e41ab06dfa867e66c4ed9d55dd
Clark Weckmann <15677451+clarkhacks@users.noreply.github.com>
2022-09-12 12:24:34 -0500

    fixed og temp

--- components/Container.js
+++ components/Container.js
@@ -38,7 +38,7 @@ const Container = ({ children, layout, fullWidth, ...customMeta }) => {
         />
         <meta
           property="og:image"
-          content={`${BLOG.ogImageGenerateURL}/?url=` + meta.slug ? `${url}${meta.slug}` : url}
+          content={`${BLOG.ogImageGenerateURL}/?url=${url}`}
         />
         <meta property="og:type" content={meta.type} />
         <meta name="twitter:card" content="summary_large_image" />
@@ -46,7 +46,7 @@ const Container = ({ children, layout, fullWidth, ...customMeta }) => {
         <meta name="twitter:title" content={meta.title} />
         <meta
           name="twitter:image"
-          content={`${BLOG.ogImageGenerateURL}/?url=}` + meta.slug ? `${url}${meta.slug}` : url}
+          content={`${BLOG.ogImageGenerateURL}/?url=${url}`}
         />
         {meta.type === 'article' && (
           <>
