0a3c2c32081e64d2424ea0a25350f3b34b7e9187
Clark Weckmann <15677451+clarkhacks@users.noreply.github.com>
2022-09-14 11:55:34 -0500

    window load wiat

--- public/linkTrack.js
+++ public/linkTrack.js
@@ -1,8 +1,10 @@
-// get all anchor tags with class notion-link
-var links = document.querySelectorAll('.notion-link')
-// prefix all hrefs with https://out.clark.today/?link= if it doesn't start with /
-for (var i = 0; i < links.length; i++) {
-    if (links[i].href.indexOf('/') !== 0) {
-        links[i].href = 'https://out.clark.today/?link=' + links[i].href;
+window.onload = function () {
+  // get all anchor tags with class notion-link
+  var links = document.querySelectorAll(".notion-link");
+  // prefix all hrefs with https://out.clark.today/?link= if it doesn't start with /
+  for (var i = 0; i < links.length; i++) {
+    if (links[i].href.indexOf("/") !== 0) {
+      links[i].href = "https://out.clark.today/?link=" + links[i].href;
     }
-}
+  }
+};
